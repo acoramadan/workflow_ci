dee09769cfc908d65e04a80a5621bc38dccf2578
acoramadan <laxmiwatimaani@gmail.com>
2025-12-22 22:50:01 +0800

    run local

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,35 +14,32 @@ def main(data_dir, n_estimators, max_depth):
     y_train = joblib.load(os.path.join(data_dir, "y_train.pkl"))
     y_test  = joblib.load(os.path.join(data_dir, "y_test.pkl"))
 
-    mlflow.set_experiment("RF-CI-Project")
-
-    with mlflow.start_run():
-        model = RandomForestClassifier(
+    model = RandomForestClassifier(
             n_estimators=n_estimators,
             max_depth=max_depth,
             random_state=42
-        )
-        model.fit(X_train, y_train)
+    )
+    model.fit(X_train, y_train)
 
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-        mlflow.log_param("n_estimators", n_estimators)
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_metric("accuracy", acc)
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_metric("accuracy", acc)
 
-        mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, "model")
 
-        cm = confusion_matrix(y_test, y_pred)
-        plt.imshow(cm)
-        plt.savefig("confusion_matrix.png")
-        plt.close()
-        mlflow.log_artifact("confusion_matrix.png")
+    cm = confusion_matrix(y_test, y_pred)
+    plt.imshow(cm)
+    plt.savefig("confusion_matrix.png")
+    plt.close()
+    mlflow.log_artifact("confusion_matrix.png")
 
-        report = classification_report(y_test, y_pred)
-        with open("classification_report.txt", "w") as f:
-            f.write(report)
-        mlflow.log_artifact("classification_report.txt")
+    report = classification_report(y_test, y_pred)
+    with open("classification_report.txt", "w") as f:
+        f.write(report)
+    mlflow.log_artifact("classification_report.txt")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
