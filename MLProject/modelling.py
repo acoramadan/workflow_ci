045712d92d58043361899cd919c7738469c8908e
acoramadan <laxmiwatimaani@gmail.com>
2025-12-23 18:47:53 +0800

    updating ci cd

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,24 +4,34 @@ import joblib
 import mlflow
 import mlflow.sklearn
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-def main(data_dir, n_estimators, max_depth):
-    X_train = joblib.load(os.path.join(data_dir, "X_train.pkl"))
-    X_test  = joblib.load(os.path.join(data_dir, "X_test.pkl"))
-    y_train = joblib.load(os.path.join(data_dir, "y_train.pkl"))
-    y_test  = joblib.load(os.path.join(data_dir, "y_test.pkl"))
+def main(n_estimators, max_depth):
+    df = pd.read_csv("hf://datasets/nikesh66/Sarcasm-dataset/sarcasm_tweets.csv")
 
-    model = RandomForestClassifier(
-        n_estimators=n_estimators,
-        max_depth=max_depth,
-        random_state=42
+    X = df["Tweet"].astype(str)
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
     )
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    pipeline = Pipeline([
+        ("tfidf", TfidfVectorizer(ngram_range=(1, 3))),
+        ("rf", RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            random_state=42
+        ))
+    ])
+
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
 
@@ -29,18 +39,8 @@ def main(data_dir, n_estimators, max_depth):
     mlflow.log_param("max_depth", max_depth)
     mlflow.log_metric("accuracy", acc)
 
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(pipeline, "model")
 
-    cm = confusion_matrix(y_test, y_pred)
-    plt.imshow(cm)
-    plt.savefig("confusion_matrix.png")
-    plt.close()
-    mlflow.log_artifact("confusion_matrix.png")
-
-    report = classification_report(y_test, y_pred)
-    with open("classification_report.txt", "w") as f:
-        f.write(report)
-    mlflow.log_artifact("classification_report.txt")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
